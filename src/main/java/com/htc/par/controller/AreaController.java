36ada73640bf6d20c5d533b7c1b25cea1ccbb0ad
Allen Selvaraj <allenscorp@Allens-MacBook-Pro.local>
2019-02-20 12:01:09 -0600

    Adding Area controller to branch

--- src/main/java/com/htc/par/controller/AreaController.java
+++ src/main/java/com/htc/par/controller/AreaController.java
@@ -2,6 +2,12 @@ package com.htc.par.controller;
 
 import org.springframework.stereotype.Controller;
 
+/*
+ * @ Author - Allen
+ * Controller to handle Capability Area information 
+ * 
+ */
+
 @Controller
 public class AreaController {
 
