184b3f286a03b2bdad3210fbe5e7d69b6a1f628b
Ram Durvasula <ramsdurvasula@gmail.com>
2019-02-21 13:39:23 -0600

    Updating Recruiter classes

--- src/main/java/com/htc/par/controller/RecruiterController.java
+++ src/main/java/com/htc/par/controller/RecruiterController.java
@@ -3,30 +3,99 @@ package com.htc.par.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.htc.par.exceptions.ResourceDuplicateException;
+import com.htc.par.exceptions.ResourceNotCreatedException;
+import com.htc.par.exceptions.ResourceNotDeletedException;
+import com.htc.par.exceptions.ResourceNotFoundException;
+import com.htc.par.exceptions.ResourceNotUpdatedException;
 import com.htc.par.repository.RecruiterRepository;
+import com.htc.par.service.SkillService;
 import com.htc.par.to.RecruiterTO;
+import com.htc.par.to.SkillTO;
 
-/* @author - Ram
- * Controller to handle Recruiter's information.
+/**
+ * @author Ram
+ * Rest Controller to handle Recruiter's information.
  */
+
 @RestController
+@RequestMapping("/par")
 public class RecruiterController {
 	
 	@Autowired
 	RecruiterRepository recruiterRepository;
+	
+	@Autowired
+	private RecruiterService recruiterService;
 
-	@GetMapping("/par/recruiter")
-	public List<RecruiterTO> getRecruiters() {
-		return null;
+	/**
+	 * Request handler to get Recruiter by id
+	 * @param recruiterId
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
+	@GetMapping("/recruiter/{recruiterId}")
+	public ResponseEntity<RecruiterTO> getRecruiterById(@PathVariable("recruiterId") int recruiterId) throws ResourceNotFoundException {
+		return ResponseEntity.ok(skillService.getSkillById(skillId));
 	}
 
-	@PostMapping("/par/recruiter")
-	public List<RecruiterTO> getRecruitersInfo() {
-		return null;
+	/**
+	 * Request handler to get all skills 
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
+	@GetMapping("/skills")
+	public ResponseEntity<List<SkillTO>> getSkills() throws ResourceNotFoundException {
+		return ResponseEntity.ok(skillService.getAllSkills());
+	}
+
+	/**
+	 * Request handler to new active skill
+	 * @param skillTO
+	 * @return
+	 * @throws ResourceNotCreatedException
+	 * @throws ResourceDuplicateException
+	 */
+	@PostMapping("/skills")
+	public ResponseEntity<SkillTO> createSkill(@RequestBody SkillTO skillTO)
+			throws ResourceNotCreatedException, ResourceDuplicateException {
+		return ResponseEntity.created(null).body(skillService.createSkill(skillTO));
+	}
+
+	/**
+	 * Request handler to update active skill
+	 * @param skillTO
+	 * @return
+	 * @throws ResourceNotFoundException
+	 * @throws ResourceNotUpdatedException
+	 */
+	@PutMapping("/skills")
+	public ResponseEntity<SkillTO> updateProduct(@RequestBody SkillTO skillTO)
+			throws ResourceNotFoundException, ResourceNotUpdatedException {
+		return ResponseEntity.ok(skillService.updateSkill(skillTO));
+	}
+
+	/**
+	 * Request handler to delete skill
+	 * @param skillId
+	 * @return
+	 * @throws ResourceNotFoundException
+	 * @throws ResourceNotDeletedException
+	 */
+	@DeleteMapping("/skills/{skillId}")
+	public ResponseEntity<Boolean> deleteProduct(@PathVariable("skillId") int skillId)
+			throws ResourceNotFoundException, ResourceNotDeletedException {
+		return ResponseEntity.ok(skillService.deleteSkill(skillId));
 	}
 	
 }
