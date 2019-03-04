24d09868a9378d2343d95890b3971a05d5b1090c
unknown <rahman.fazzu@gmail.com>
2019-03-04 11:11:38 -0600

    Added PARMaster Repository

---
+++ src/main/java/com/htc/par/repository/PARMasterRepository.java
@@ -0,0 +1,17 @@
+package com.htc.par.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.htc.par.entity.PARRole;
+
+/**
+ * Repository for PAR Role Entity
+ *
+ */
+
+@Repository
+public interface PARMasterRepository extends JpaRepository<PARRole, Integer> {
+	
+
+}
