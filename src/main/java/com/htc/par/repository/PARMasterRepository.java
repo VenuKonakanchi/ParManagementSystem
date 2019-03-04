fbf62c17535e0971dc4311c23722ebaadd0bbde7
unknown <rahman.fazzu@gmail.com>
2019-03-04 11:31:46 -0600

    Edited PARMasterRepository

--- src/main/java/com/htc/par/repository/PARMasterRepository.java
+++ src/main/java/com/htc/par/repository/PARMasterRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.htc.par.entity.PARMaster;
-import com.htc.par.entity.PARRole;
 
 /**
  * Repository for PAR Role Entity
